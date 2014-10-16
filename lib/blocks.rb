8a2a09444ff542ded46b2d26f27d729419ae9be3
Kenner Kliemann <kenner.hp@gmail.com>
2014-10-16 11:10:36 -0300

    Blocks

---
+++ lib/blocks.rb
@@ -0,0 +1,19 @@
+5.times { |t| puts t }
+
+def do_something(&block)
+  puts block.call
+end
+
+do_something { 10.times { |t| puts t*t } }
+
+def do_something(&block)
+  if block_given?
+    puts yield
+  else
+    puts "Try again"
+  end
+end
+
+do_something
+do_something { "Hello World" }
+
